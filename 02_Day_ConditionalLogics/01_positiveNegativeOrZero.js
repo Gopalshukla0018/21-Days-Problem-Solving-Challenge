9a879d2801ea50dc7ec692ad1ab2d04d80cc59e6
Gopal Shukla <gopalshukla0018@gmail.com>
2026-02-08 23:58:38 +0530

    Refactor positiveNegativeOrZero function and clean up commented-out code

--- 02_Day_ConditionalLogics/01_positiveNegativeOrZero.js
+++ 02_Day_ConditionalLogics/01_positiveNegativeOrZero.js
@@ -1,13 +1,28 @@
 // check number is positive, negative or zero
 
-
 // basic sol..
-console.time("Execution Time");
-let num = -5;
-if (num > 0) {
-  console.log(num, "is a positive number.");
-} else if (num < 0) {
-  console.log(num, "is a negative number.");
-} else {
-  console.log(num, "is zero.");
+// console.time("Execution Time");
+// let num = -5;
+// if (num > 0) {
+//   console.log(num, "is a positive number.");
+// } else if (num < 0) {
+//   console.log(num, "is a negative number.");
+// } else {
+//   console.log(num, "is zero.");
+// }
+
+// console.timeEnd("Execution Time");
+
+function positiveNegativeOrZero(num) {
+  if (num > 0) {
+    return "positive";
+  } else if (num < 0) {
+    return "negative";
+  } else {
+    return "zero";
+  }
 }
+
+console.log(positiveNegativeOrZero(1));
+console.log(positiveNegativeOrZero(-5));
+console.log(positiveNegativeOrZero(0));
