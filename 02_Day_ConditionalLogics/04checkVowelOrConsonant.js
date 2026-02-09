ea433a1a49417f3edf763416a66a3d2aecc0a911
Gopal Shukla <gopalshukla0018@gmail.com>
2026-02-09 10:58:33 +0530

    Refactor checkVowelOrConsonant function to use array for vowels and clean up commented-out code

--- 02_Day_ConditionalLogics/04checkVowelOrConsonant.js
+++ 02_Day_ConditionalLogics/04checkVowelOrConsonant.js
@@ -4,25 +4,38 @@
 // Output: Vowel
 //  Combines logical OR (||) operator and string comparisons.
 
-function checkVowelOrConsonant(char){
-    
-    let string=char.toLocaleString()
-    let lowerChar=string.toLowerCase();
-    if(lowerChar == 'a' || lowerChar=='i' || lowerChar == 'e' || lowerChar =='o' || lowerChar=='u' )
-    {
-        return `${char} is a vowel`
-    }
-  else if(/[a-z]/.test(lowerChar)){
-      return `${char} is a Consonant`
+// function checkVowelOrConsonant(char){
+
+//     let lowerChar=string.toLowerCase();
+//     if(lowerChar == 'a' || lowerChar=='i' || lowerChar == 'e' || lowerChar =='o' || lowerChar=='u' )
+//     {
+//         return `${char} is a vowel`
+//     }
+//   else if(/[a-z]/.test(lowerChar)){
+//       return `${char} is a Consonant`
+//   }
+//     else{
+//           return `${char} is not a Valid Char`
+//     }
+
+// }
+
+// console.log(checkVowelOrConsonant('a'))
+// console.log(checkVowelOrConsonant('b'))
+// console.log(checkVowelOrConsonant(5))
+
+// 2nd approach
+function checkVowelOrConsonant(char) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.includes(char.toLowerCase())) {
+    return `${char} is a vowel`;
+  } else if (/[a-z]/.test(char.toLowerCase())) {
+    return `${char} is a Consonant`;
+  } else {
+    return `${char} is not a Valid Char`;
   }
-    else{
-          return `${char} is not a Valid Char`
-    }
-    
 }
 
-
-
-console.log(checkVowelOrConsonant('a'))
-console.log(checkVowelOrConsonant('b'))
-console.log(checkVowelOrConsonant(5))
+console.log(checkVowelOrConsonant("a"));
+console.log(checkVowelOrConsonant("b"));
+console.log(checkVowelOrConsonant("5"));
